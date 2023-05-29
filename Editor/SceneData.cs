a421c3f4a818d0dc315122cab6a2ea6d32c2c624
Theo Farnole <t.farnole@dowino.com>
2023-05-29 17:26:35 +0200

    ADD select button

--- Editor/SceneData.cs
+++ Editor/SceneData.cs
@@ -93,7 +93,7 @@
             }
             else
             {
-                int clickedButton = GUILayout.Toolbar(-1, new string[] { "Open", "+" }, GUILayout.Width(WIDTH_PX_BUTTONS * 2));
+                int clickedButton = GUILayout.Toolbar(-1, new string[] { "Open", "+", "Select" }, GUILayout.Width(WIDTH_PX_BUTTONS * 2));
 
                 switch (clickedButton)
                 {
@@ -103,6 +103,10 @@
 
                     case 1:
                         sceneAsset.OpenScene(OpenSceneMode.Additive);
+                        break;
+
+                    case 2:
+                        Selection.activeObject = AssetDatabase.LoadMainAssetAtPath(sceneAsset.Path);
                         break;
                 }
             }
