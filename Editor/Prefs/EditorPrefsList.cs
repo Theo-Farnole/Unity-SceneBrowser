4a13d917d1b449a7314236a1c7a90ed7ce886806
Theo Farnole <t.farnole@dowino.com>
2023-06-01 17:28:27 +0200

    FIX persistence wasn't working + folders architecture cleanup + REMOVE resources

---
+++ Editor/Prefs/EditorPrefsList.cs
@@ -0,0 +1,44 @@
+﻿namespace TF.SceneBrowser.Editor
+{
+    using System.Collections.Generic;
+    using UnityEditor;
+
+    internal class EditorPrefsList
+    {
+        public readonly string key;
+        public readonly char separator;
+
+        private readonly List<string> _data;
+
+        public EditorPrefsList(string key, char separator)
+        {
+            this.key = key;
+            this.separator = separator;
+
+            string fromSaveString = EditorPrefs.GetString(key);
+
+            string[] scenesPath = fromSaveString.Split(separator);
+            _data = new List<string>(scenesPath);
+        }
+
+        public void Add(string value)
+        {
+            _data.Add(value);
+            SaveChanges();
+        }
+
+        public void Remove(string value)
+        {
+            _data.Remove(value);
+            SaveChanges();
+        }
+
+        public bool Contains(string value) => _data.Contains(value);
+
+        private void SaveChanges()
+        {
+            string joinedFavoritesScenesPaths = string.Join(separator.ToString(), _data);
+            EditorPrefs.SetString(key, joinedFavoritesScenesPaths);
+        }
+    }
+}
---
+++ Editor/Prefs/Favorites.cs
@@ -0,0 +1,37 @@
+﻿namespace TF.SceneBrowser.Editor
+{
+    using UnityEngine;
+
+    internal static class Favorites
+    {
+        private const string PREFS_FAVORITE_SCENE = "SceneBrowser_FavoriteScene";
+        private const char SEPARATOR = ';';
+        private static readonly EditorPrefsList _favoritesScenesPath = new(PREFS_FAVORITE_SCENE, SEPARATOR);
+
+        public static void FavoriteScene(SceneData sceneAsset)
+        {
+            if (IsSceneFavorite(sceneAsset) == true)
+            {
+                Debug.LogWarningFormat("Scene {0} is already favorite. Can't favorite it again.", sceneAsset.Name);
+                return;
+            }
+
+            _favoritesScenesPath.Add(sceneAsset.Path);
+        }
+
+        public static void UnfavoriteScene(SceneData sceneAsset) => _favoritesScenesPath.Remove(sceneAsset.Path);
+        public static bool IsSceneFavorite(SceneData sceneAsset) => _favoritesScenesPath.Contains(sceneAsset.Path);
+
+        public static void ToggleFavorite(SceneData sceneAsset)
+        {
+            if (IsSceneFavorite(sceneAsset) == true)
+            {
+                UnfavoriteScene(sceneAsset);
+            }
+            else
+            {
+                FavoriteScene(sceneAsset);
+            }
+        }
+    }
+}
